652f7a53b20387598ee2fd1e28f41093a4e3ea42
Cameron McCarthy <cameronmccarthy@169-231-131-59.wireless.ucsb.edu>
2015-10-12 13:29:46 -0700

    cm: Added an edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
 
   def new
+    # Render the login view
+  end
 
+  def show
+    @user = User.find_by(:id => params[:id])
+    render "show"
+    # Render the user info page view
+  end
+
+  def edit
+    @user = User.find_by(:id => params[:id])
+    render "edit"
+    # Allow a user to change the data about their account
+  end
+
+  def update
+    user = User.find_by(:id => params[:id])
+    user.update(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/user/:id'
+    end
   end
 
   def create
