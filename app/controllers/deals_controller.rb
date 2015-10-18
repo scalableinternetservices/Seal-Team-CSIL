110cf6524007b782aac5991838c04955dec1658d
Cameron McCarthy <cameronmccarthy@169-231-142-95.wireless.ucsb.edu>
2015-10-17 21:00:53 -0700

    cam: Added a view to view all deals as well as edit them. Need to be able to remove them but I wanted to push before tonight is done.

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,22 +1,46 @@
 class DealsController < ApplicationController
 
-  before_filter :authorize, :only => [:create, :new]
+  before_filter :authorize, :only => [:create, :new, :edit, :update]
 
   def create
     deal = Deal.new(deals_params)
     deal.user_id = current_user.id
     if deal.save!
       flash[:success] = "Deal has been created!"
-      redirect_to '/'
+      redirect_to "/users/#{current_user.id}/deals"
     else
       flash[:error] = "Something went wrong in creating the deal!"
-      redirect_to '/signup'
+      redirect_to "/users/#{current_user.id}/deals"
     end
   end 
 
   def new
-    @user = current_user
+    @user = User.find_by(id: params[:id])
     render 'new'
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @deals = @user.deals
+    render 'show'
+  end
+
+  def edit
+    @user = User.find_by(id: params[:user_id])
+    @deal = Deal.find_by(id: params[:deal_id])
+    render 'edit'
+  end
+
+  def update
+    deal = Deal.find_by(:id => params[:deal_id])
+    deal.update(deals_params)
+    if deal.save!
+      flash[:success] = "Your deal has been updated."
+      redirect_to "/users/#{params[:user_id]}/deals"
+    else
+      flash[:error] = "Something went wrong. Change this when specific validations are created."
+      redirect_to "/users/#{params[:user_id]}/deals"
+    end
   end
 
   private
