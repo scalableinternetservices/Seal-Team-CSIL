0b5f085d5c46d4519766ed1e27173575317026f4
Jenna Cryan <jrcryan@cs.ucsb.edu>
2015-10-25 20:35:05 -0700

    added validation for 10 digit #s (normalizes the leading +1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ActiveRecord::Base
   validates         :password, confirmation: true
   validate          :validate_address
   validates         :email, email: true
-  validates         :phone_number, phony_plausible: true
+  phony_normalize   :phone_number, default_country_code: 'US'
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_secure_password
-  
+
+
   def validate_address
     if StreetAddress::US.parse(address).nil?
       errors.add(address, "Need a valid address")
