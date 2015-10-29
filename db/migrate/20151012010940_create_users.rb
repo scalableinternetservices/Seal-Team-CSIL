96e329a28aa2842075c74665cdd0e7ffebeb1e48
Jenna Cryan <jrcryan@cs.ucsb.edu>
2015-10-29 11:10:12 -0700

    changed password to password_digest

--- db/migrate/20151012010940_create_users.rb
+++ db/migrate/20151012010940_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.string :food_type
       t.string :address
       t.string :phone_number
