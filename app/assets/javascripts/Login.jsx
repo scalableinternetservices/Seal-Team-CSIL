b54d6d7b5e766bfc5bb1264510b03d9dcbc833bd
Cameron McCarthy <cameronmccarthy@169-231-141-248.wireless.ucsb.edu>
2015-10-29 14:15:01 -0700

    cam: for ec2 demo

---
+++ app/assets/javascripts/Login.jsx
@@ -0,0 +1,15 @@
+var Login = React.createClass({
+  render:  function() {
+    return(
+      <h1>Login</h1>
+
+      <%= form_tag '/login' do %>
+
+        Email: <%= text_field_tag :email %>
+        Password: <%= password_field_tag :password %>
+        <%= submit_tag "Submit" %>
+
+      <% end %>
+    );
+  } 
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,30 +21,48 @@ class UsersController < ApplicationController
   def update
     user = User.find_by(:id => params[:id])
     user.update(user_params)
-    user.save!
-    session[:user_id] = user.id
-    flash[:success] = "Your information has been updated."
-    redirect_to "/"
-    rescue
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Your information has been updated."
+      redirect_to "/"
+    else
       flash[:error] = "Something went wrong. Change this when specific validations are created."
       redirect_to "/users/#{:id}"
+    end
   end
 
   def create
-    user = User.new(user_params)
-    user.save!
-    session[:user_id] = user.id
-    flash[:success] = "Account has been created!"
-    redirect_to "/"
-    rescue
+    puts "name is:\n"
+    puts user_params[:street_address]
+    user = User.new(formatted_user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Account has been created!"
+      redirect_to "/"
+    else
       flash[:error] = "Something went wrong in creating the account!"
       redirect_to '/signup'
+    end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :food_type, :address, :phone_number, :hours, :delivery, :website, :avatar)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :address, :phone_number, :website, :avatar)
+    end
+
+    def formatted_user_params
+      form_params = params.require(:user).permit(:name, :email, :password, :password_confirmation, :street_address, :city, :zip_code, :state, :phone_number, :website, :avatar)
+      full_address = form_params[:street_address] + ' ' + form_params[:city] + ' ' + form_params[:zip_code] + ' ' + form_params[:state]
+      form_params.except!('street_address', 'city', 'zip_code', 'state')
+      form_params[:address] = full_address
+      form_params[:phone_number] = format_phone_number(form_params[:phone_number])
+      form_params  
+    end
+
+    def format_phone_number(phone_number)
+      formatted = phone_number.tr('^0-9', '')
+      formatted = formatted.length == 10 ? '1' + formatted : formatted
     end
 
 end
