0e682f24ad3f35b9d30e25d0e69f045e65121d82
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2015-11-11 17:20:20 -0800

    seeding 100 users instead of 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 FOOD_TYPES = [ 'American','Mexican','Italian','Asian','French' ]
-num_users = 10
+num_users = 100
 1.upto(num_users) do |user_number|
 	User.create!(name: "user#{user_number}", email: "user#{user_number}@foodfinder.com", password: "password#{user_number}", password_confirmation: "password#{user_number}", phone_number: "18058938000", food_type: FOOD_TYPES.sample, address: "Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117")
 end
