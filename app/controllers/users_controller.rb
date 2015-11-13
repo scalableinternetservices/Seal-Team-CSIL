932dfa238c73bc7938d5820706cd5fa5133d59de
Jenna Cryan <jrcryan@cs.ucsb.edu>
2015-11-12 17:00:38 -0800

    added lat long functionality to new user and map filtering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,12 +59,19 @@ class UsersController < ApplicationController
     end
 
     def formatted_user_params
-      form_params = params.require(:user).permit(:name, :email, :password, :password_confirmation, :street_address, :city, :zip_code, :state, :phone_number, :website, :avatar)
-      full_address = form_params[:street_address] + ' ' + form_params[:city] + ' ' + form_params[:zip_code] + ' ' + form_params[:state]
-      form_params.except!('street_address', 'city', 'zip_code', 'state')
-      form_params[:address] = full_address
+      form_params = params.require(:user).permit(:name, :email, :password, :password_confirmation, :latitude, :longitude, :street_address, :city, :zip_code, :state, :phone_number, :website, :avatar)
+
+      if form_params[ :latitude ].present? and form_params[ :longitude ].present?
+        lat_lng = form_params[ :latitude ] + ',' + form_params[ :longitude ]
+        form_params.except!('latitude', 'longitude', 'street_address', 'city', 'zip_code', 'state')
+        form_params[ :address ] = Geocoder.search( lat_lng )[0].data["formatted_address"]
+      else
+        full_address = form_params[:street_address] + ' ' + form_params[:city] + ' ' + form_params[:zip_code] + ' ' + form_params[:state]
+        form_params.except!('latitude', 'longitude', 'street_address', 'city', 'zip_code', 'state')
+        form_params[:address] = full_address
+      end
       form_params[:phone_number] = format_phone_number(form_params[:phone_number])
-      form_params  
+      form_params
     end
 
     def format_phone_number(phone_number)
