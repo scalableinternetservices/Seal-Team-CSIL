52dc7bdc4d53807f349a7452e2c8f0bf2a46497d
Jenna Cryan <jrcryan@cs.ucsb.edu>
2015-11-12 16:14:44 -0800

    jc: added functionality for adding deal by lat/long coords

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -5,12 +5,13 @@ class DealsController < ApplicationController
   def create
     deal = Deal.new(formatted_deal_params, views: 0, shares: 0, purchases: 0)
     deal.user_id = current_user.id
-    deal.save!
+    if deal.save
     flash[:success] = "Deal has been created!"
     redirect_to "/users/#{current_user.id}/deals"
-    rescue
+    else
       flash[:error] = "Something went wrong in creating the deal!"
       redirect_to "/users/#{current_user.id}/deals"
+    end
   end 
 
   def new
@@ -56,7 +57,7 @@ class DealsController < ApplicationController
     flash[:success] = "Your deal has been deleted."
     redirect_to "/users/#{params[:user_id]}/deals"
     rescue
-      flash[:error] = "Something went wrong. Change this when specific validations are created."
+      flash[:error] = "Something went wrong when deleting the deal."
       redirect_to "/users/#{params[:user_id]}/deals"
   end
 
@@ -69,11 +70,18 @@ class DealsController < ApplicationController
   private
 
     def formatted_deal_params
-      form_params = params.require(:deal).permit(:food_name, :description, :street_address, :city, :zip_code, :state, :deal_type, :start_time, :end_time, :food_type, :avatar)
-      full_address = form_params[:street_address] + ' ' + form_params[:city] + ' ' + form_params[:zip_code] + ' ' + form_params[:state]
-      form_params.except!('street_address', 'city', 'zip_code', 'state')
-      form_params[:address] = full_address
-      form_params  
+        form_params = params.require(:deal).permit(:food_name, :description, :latitude, :longitude, :street_address, :city, :zip_code, :state, :deal_type, :start_time, :end_time, :food_type, :avatar)
+
+        if form_params[ :latitude ].present? and form_params[ :longitude ].present?
+          lat_lng = form_params[ :latitude ] + ',' + form_params[ :longitude ]
+          form_params.except!('latitude', 'longitude', 'street_address', 'city', 'zip_code', 'state')
+          form_params[ :address ] = Geocoder.search( lat_lng )[0].data["formatted_address"]
+        else
+          full_address = form_params[:street_address] + ' ' + form_params[:city] + ' ' + form_params[:zip_code] + ' ' + form_params[:state]
+          form_params.except!('latitude', 'longitude', 'street_address', 'city', 'zip_code', 'state')
+          form_params[:address] = full_address
+      end
+      form_params
     end
 
     def deals_params
