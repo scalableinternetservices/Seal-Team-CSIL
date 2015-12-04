0f42c920988ba2ebacd33793e3c92d07c2e92f24
camsb <cameronmccarthy@169-231-142-237.wireless.ucsb.edu>
2015-12-04 13:19:00 -0800

    cam: Fixed seeding so geocoding doesn't affect it.

--- app/helpers/timeline_helper.rb
+++ app/helpers/timeline_helper.rb
@@ -2,8 +2,4 @@ module TimelineHelper
   def cache_key_for_timeline_deal(deal)
     "deal/#{deal.id}/#{deal.updated_at}"
   end
-  
-  def cache_key_for_timeline_deal_table
-    "timeline_deal_table/#{User.maximum(:updated_at)}/#{Deal.maximum(:updated_at)}"
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Deal.skip_callback(:address, :geocode)
+connection = ActiveRecord::Base.connection();
+
 FOOD_TYPES = [ 'American','Mexican','Italian','Asian','French' ]
 DEAL_TYPES = [ 'Free','Buy One Get One Free','Buy One Get One Half Off','Flash Deal' ]
+FOOD_NAMES = [ 'Cheese Pizza', 'Hamburger', 'Cheese Burger', 'Hotdog', 'Icee', 'Pasta', 'Sasuage', 'Chicken Bowl']
 #create 10,000 users
-num_users = 10000
-food_num = 0
-1.upto(num_users) do |user_number|
-	User.create!(name: "user#{user_number}", 
+num_users = 100
+deal_id_count = 1
+
+num_users.times do |user_number|
+	a = User.create!(name: "user#{user_number}", 
 		email: "user#{user_number}@foodfinder.com", 
 		password: "password#{user_number}", 
 		password_confirmation: "password#{user_number}", 
 		phone_number: "18058938000", 
 		food_type: FOOD_TYPES.sample, 
 		address: "Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117")
-	1.upto(10) do |deal_number|
-		##fix address for deals
-	Deal.create!(food_name: "food#{food_num+=1}", 
-		address: "Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117",
-		food_type: FOOD_TYPES.sample, 
-		start_time: "8:00",
-		end_time: "9:00",
-		user_id: "#{user_number}",
-		deal_type: DEAL_TYPES.sample)
+	10.times do |i|
+  	Deal.connection.execute "insert into deals 
+  	(id, food_name, latitude, longitude, address, food_type, start_time, end_time, user_id, deal_type, created_at, updated_at) 
+  	values (#{deal_id_count}, '#{FOOD_NAMES.sample}', #{rand * 0.037484 + 34.409627}, #{rand * 0.193776 - 119.878732}, 'Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117', '#{FOOD_TYPES.sample}', NOW(), NOW(), #{a.id}, '#{DEAL_TYPES.sample}', NOW(), NOW());"
+  	deal_id_count += 1
 	end
-
 end
 
