41f3c4394a89e7ec8826e3f13efc6a00bd98e427
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2015-12-04 01:43:31 -0800

    added seeding 10k users 100k deals, lots of over query limits and timelineview bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 FOOD_TYPES = [ 'American','Mexican','Italian','Asian','French' ]
-num_users = 100
+DEAL_TYPES = [ 'Free','Buy One Get One Free','Buy One Get One Half Off','Flash Deal' ]
+#create 10,000 users
+num_users = 10000
+food_num = 0
 1.upto(num_users) do |user_number|
-	User.create!(name: "user#{user_number}", email: "user#{user_number}@foodfinder.com", password: "password#{user_number}", password_confirmation: "password#{user_number}", phone_number: "18058938000", food_type: FOOD_TYPES.sample, address: "Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117")
+	User.create!(name: "user#{user_number}", 
+		email: "user#{user_number}@foodfinder.com", 
+		password: "password#{user_number}", 
+		password_confirmation: "password#{user_number}", 
+		phone_number: "18058938000", 
+		food_type: FOOD_TYPES.sample, 
+		address: "Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117")
+	1.upto(10) do |deal_number|
+		##fix address for deals
+	Deal.create!(food_name: "food#{food_num+=1}", 
+		address: "Harold Frank Hall, 1138 Lagoon Rd, Isla Vista, CA 93117",
+		food_type: FOOD_TYPES.sample, 
+		start_time: "8:00",
+		end_time: "9:00",
+		user_id: "#{user_number}",
+		deal_type: DEAL_TYPES.sample)
+	end
+
 end
+
