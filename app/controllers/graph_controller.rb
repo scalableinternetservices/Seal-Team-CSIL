ba3662e05fd67c1d324c1ef8d0a3f57774fa612f
camsb <cameronmccarthy@169-231-142-237.wireless.ucsb.edu>
2015-12-04 18:56:44 -0800

    cam: SQL query version of querying removed

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -7,27 +7,68 @@ class GraphController < ApplicationController
   end
 
   def load_local_deals
-    # num = params[:num]
+    # # num = params[:num]
+    # lat = params[:lat].to_f
+    # lng = params[:lng].to_f
+    # # deals = Deal.where("sqrt(power(#{lat}-latitude,2) + power(#{lng}-longitude,2)) < 0.0164").limit(num)
+    # # deals = Deal.where("sqrt(power(#{lat}-latitude,2) + power(#{lng}-longitude,2)) < 0.0164")
+    # hash = Gmaps4rails.build_markers(deals) do |deal, marker|
+
+    #   marker.lat deal.latitude
+    #   marker.lng deal.longitude
+    #   window = deal.user.avatar.path ? createInfoWindowWithImage(deal) : createInfoWindowWithoutImage(deal)
+    #   marker.infowindow window
+    # end
+    # respond_to do |format|
+    #   format.json { render :json => hash, :layout => false}
+    # end
     lat = params[:lat].to_f
     lng = params[:lng].to_f
-    # deals = Deal.where("sqrt(power(#{lat}-latitude,2) + power(#{lng}-longitude,2)) < 0.0164").limit(num)
-    deals = Deal.where("sqrt(power(#{lat}-latitude,2) + power(#{lng}-longitude,2)) < 0.0164")
+    deals = Deal.all
     hash = Gmaps4rails.build_markers(deals) do |deal, marker|
-
-      marker.lat deal.latitude
-      marker.lng deal.longitude
-      window = deal.user.avatar.path ? createInfoWindowWithImage(deal) : createInfoWindowWithoutImage(deal)
-      marker.infowindow window
+      #TODO Add validation to deals so that they must have lat and lng, then get rid of this check
+      if deal.latitude.present?
+        if coordinate_distance([lat,lng],[deal.latitude, deal.longitude]) < 1600
+          marker.lat deal.latitude
+          marker.lng deal.longitude
+          window = deal.user.avatar.path ? createInfoWindowWithImage(deal) : createInfoWindowWithoutImage(deal)
+          marker.infowindow window
+          # marker.infowindow createInfoWindow(deal)
+        end
+      end
     end
+    #puts hash.to_json
     respond_to do |format|
       format.json { render :json => hash, :layout => false}
     end
+    #render hash, :layout => false
   end
 
   def save_user_location
     render :nothing => true
     Rails.cache.fetch('lat') {params[:lat]}
     Rails.cache.fetch('lng') {params[:lng]}
+  end
+
+  private
+
+  def coordinate_distance(loc1, loc2)
+    #puts loc1
+    #puts loc2
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    rm * c # Delta in meters
   end
 
 end
--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,33 +1,90 @@
 class TimelineController < ApplicationController
 
   def show
+    # @lat = Rails.cache.fetch('lat').to_f
+    # @lng = Rails.cache.fetch('lng').to_f
+    # @distance_miles = 1.0
+    # # @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles}").paginate(:page => params[:page], :per_page => 10)
+    # @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles}")
+
+    # render 'show'
+    # Render timeline and pass all necessary info to the view.
+    @deals_within_proximity = []
     @lat = Rails.cache.fetch('lat').to_f
     @lng = Rails.cache.fetch('lng').to_f
     @distance_miles = 1.0
-    # @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles}").paginate(:page => params[:page], :per_page => 10)
-    @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles}")
-
+    distance_meters = @distance_miles * 1609.34
+    Deal.all.each do |deal|
+      if coordinate_distance([deal.latitude, deal.longitude],[@lat,@lng]) <= distance_meters
+        @deals_within_proximity.append(deal)
+      end
+    end
     render 'show'
   end
 
   def load_deals
-  	@distance_miles = params[ :distance_from_address ].to_f()
+  	# @distance_miles = params[ :distance_from_address ].to_f()
+
+   #  if params[ :street_address ].present?
+   #  	location = Geocoder.search( params[ :street_address ] )
+   #  elsif params[ :lat_lng ].present?
+   #    location = Geocoder.search( params[ :lat_lng ] )
+   #  else
+   #    location = Geocoder.search( Rails.cache.fetch('lat') + ',' + Rails.cache.fetch('lng') )
+   #  end
+
+   #  @lat = location[0].latitude
+   #  @lng = location[0].longitude
+   #  # @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles} AND (#{params[:food_type] == 'Any'} OR food_type='#{params[:food_type].to_s}')").paginate(:page => params[:page], :per_page => 10)
+   #  @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles} AND (#{params[:food_type] == 'Any'} OR food_type='#{params[:food_type].to_s}')")
+
+  	# render 'show'
+    @distance_miles = params[ :distance_from_address ].to_f()
+    distance_meters = @distance_miles  * 1609.34
 
     if params[ :street_address ].present?
-    	location = Geocoder.search( params[ :street_address ] )
+      location = Geocoder.search( params[ :street_address ] )
     elsif params[ :lat_lng ].present?
       location = Geocoder.search( params[ :lat_lng ] )
     else
       location = Geocoder.search( Rails.cache.fetch('lat') + ',' + Rails.cache.fetch('lng') )
     end
 
+    @deals_within_proximity = []
     @lat = location[0].latitude
     @lng = location[0].longitude
-    # @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles} AND (#{params[:food_type] == 'Any'} OR food_type='#{params[:food_type].to_s}')").paginate(:page => params[:page], :per_page => 10)
-    @deals_within_proximity = Deal.where("sqrt(power(#{@lat}-latitude,2) + power(#{@lng}-longitude,2)) < #{0.0164 * @distance_miles} AND (#{params[:food_type] == 'Any'} OR food_type='#{params[:food_type].to_s}')")
-    
-  	render 'show'
+
+    Deal.all.each do |deal|
+      if coordinate_distance([deal.latitude, deal.longitude],[@lat,@lng]) <= distance_meters
+        if params[:food_type] == 'Any' || deal.food_type == params[:food_type]
+          @deals_within_proximity.append(deal)
+        end
+      end
+    end
+    render 'show'
   end
 
 end
+
+private
+
+
+  def coordinate_distance(loc1, loc2)
+    #puts loc1
+    #puts loc2
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    rm * c # Delta in meters
+  end
 
