3764dc12103c9cd405c591dd299aabfab335d056
camsb <cameronmccarthy@169-231-132-39.wireless.ucsb.edu>
2015-12-05 10:46:43 -0800

    cam: Moved logic from timeline view to controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     { success: 'alert-success', error: 'alert-danger', warning: 'alert-warning'}[flash_type.to_sym]
  end
 
- def flash_messages(opts = {})
+def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "text-center alert #{bootstrap_class_for(msg_type)} fade in") do
         concat content_tag(:button, 'x'.html_safe, class: 'close', data: {dismiss: 'alert'})
@@ -12,5 +12,5 @@ module ApplicationHelper
       flash.clear
     end
     nil
- end
+  end
 end
--- db/migrate/20151024205824_add_stats_to_deals.rb
+++ db/migrate/20151024205824_add_stats_to_deals.rb
@@ -1,7 +1,5 @@
 class AddStatsToDeals < ActiveRecord::Migration
   def change
     add_column :deals, :views, :integer, default: 0
-    add_column :deals, :shares, :integer, default: 0
-    add_column :deals, :purchases, :integer, default: 0
   end
 end
