ef2fea183d22ca651e88475edddbcdc5a5b80947
camsb <cameron@umail.ucsb.edu>
2015-12-06 03:49:11 -0800

    cam: No cached location in timeline view

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,8 +3,10 @@ class TimelineController < ApplicationController
   include TimelineHelper
 
   def show
-    lat = get_lat(request.remote_ip.to_s)
-    lng = get_lng(request.remote_ip.to_s)
+    # lat = get_lat(request.remote_ip.to_s)
+    # lng = get_lng(request.remote_ip.to_s)
+    lat = '34.413347'.to_f
+    lng = '-119.855441'.to_f
     distance_miles = 1.0
     @deals_within_proximity = Deal.where("sqrt(power(#{lat}-latitude,2) + power(#{lng}-longitude,2)) < #{0.0164 * distance_miles}").paginate(:page => params[:page], :per_page => 10)
     @address_data = get_address_data
@@ -18,8 +20,10 @@ class TimelineController < ApplicationController
     else
       location = Geocoder.search( get_lat(request.remote_ip.to_s) + ',' + get_lng(request.remote_ip.to_s) )
     end
-    lat = location[0].latitude
-    lng = location[0].longitude
+    # lat = location[0].latitude
+    # lng = location[0].longitude
+    lat = '34.413347'.to_f
+    lng = '-119.855441'.to_f
     @deals_within_proximity = Deal.where("sqrt(power(#{lat}-latitude,2) + power(#{lng}-longitude,2)) < #{0.0164 * distance_miles} AND (#{params[:food_type] == 'Any'} OR food_type='#{params[:food_type].to_s}')").paginate(:page => params[:page], :per_page => 10)
     @address_data = get_address_data
   	render 'show'
@@ -34,7 +38,8 @@ class TimelineController < ApplicationController
       geocoder_result = Geocoder.search( get_lat(request.remote_ip.to_s) + ',' + get_lng(request.remote_ip.to_s) )
       address_valid = geocoder_result[0].nil? ? false : true
     end
-    address_data = address_valid ? geocoder_result[ 0 ].data["formatted_address"] : Geocoder.search( get_lat(request.remote_ip.to_s) + ',' + get_lng(request.remote_ip.to_s) )[ 0 ].data["formatted_address"]
+    # address_data = address_valid ? geocoder_result[ 0 ].data["formatted_address"] : Geocoder.search( get_lat(request.remote_ip.to_s) + ',' + get_lng(request.remote_ip.to_s) )[ 0 ].data["formatted_address"]
+    address_data = ''
   end
 
 end
